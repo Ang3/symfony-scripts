e028206264b8aec97ca174a1dc3aec99e9a5a430
Joanis Rouanet <joanis.ang3@gmail.com>
2022-12-31 16:01:51 +0100

    Fixed descriptions

--- bin/sf_start.sh
+++ bin/sf_start.sh
@@ -7,13 +7,13 @@ echo -e "\033[33;1m--- STARTING APPLICATION ---\033[0m"
 if [ -f "docker-compose.yml" -o -f "docker-compose.yaml" ]
 then
   echo
-  echo -e "\033[33;1mStarting Docker containers...\033[0m"
+  echo -e "\033[33;1mStarting Docker containers\033[0m"
   docker-compose up -d
   sleep 3
 fi
 
 echo
-echo -e "\033[33;Composer installation...\033[0m"
+echo -e "\033[33;1mComposer installation\033[0m"
 composer install
 
 isDoctrineInstalled=$(cat config/bundles.php | grep -c "DoctrineBundle::class")
@@ -28,18 +28,18 @@ then
   then
     if [ "$1" = "reset" ]
     then
-      echo -e "\033[33;1mReinitializing doctrine migrations files...\033[0m"
+      echo -e "\033[33;1mReinitializing doctrine migrations files\033[0m"
       rm -rf migrations/Version*.php
       symfony console doctrine:migrations:diff
     fi
   fi
 
   echo
-  echo -e "\033[33;1mMigrating database...\033[0m"
+  echo -e "\033[33;1mMigrating database\033[0m"
   symfony console doctrine:migrations:migrate --no-interaction
 
   echo
-  echo -e "\033[33;1mRegistering fixtures...\033[0m"
+  echo -e "\033[33;1mRegistering fixtures\033[0m"
   symfony console doctrine:fixtures:load --no-interaction
 fi
 
@@ -48,12 +48,12 @@ then
   if [ "$1" = "reset" ]
   then
     echo
-    echo -e "\033[33;1mDeleting cache and logs...\033[0m"
+    echo -e "\033[33;1mDeleting cache and logs\033[0m"
     rm -rf var/cache
     rm -rf var/log
   fi
 fi
 
 echo
-echo -e "\033[33;1mStarting web server...\033[0m"
+echo -e "\033[33;1mStarting web server\033[0m"
 symfony server:start -d
--- bin/sf_stop.sh
+++ bin/sf_stop.sh
@@ -4,13 +4,13 @@ echo
 echo -e "\033[33;1m--- STOPPING APPLICATION ---\033[0m"
 
 echo
-echo -e "\033[33;1mStopping web server...\033[0m"
+echo -e "\033[33;1mStopping web server\033[0m"
 symfony server:stop
 
 # shellcheck disable=SC2166
 if [ -f "docker-compose.yml" -o -f "docker-compose.yaml" ]
 then
   echo
-  echo -e "\033[33;1mStopping Docker containers...\033[0m"
+  echo -e "\033[33;1mStopping Docker containers\033[0m"
   docker-compose down
 fi
