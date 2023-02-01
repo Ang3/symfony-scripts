a854587fd9a0be7402c36b292a704804cd7a6a5a
Joanis Rouanet <joanis.ang3@gmail.com>
2023-02-01 14:39:52 +0100

    Added custom test handler

--- bin/sf_test.sh
+++ bin/sf_test.sh
@@ -7,5 +7,12 @@ echo -e "\033[43;30;1m--- TESTING APPLICATION ---\033[0m"
 echo -e "\033[43;30;1m---------------------------\033[0m"
 echo
 
+echo
+echo -e "\033[33;1mRunning custom test handler\033[0m"
+handlerFile=bin/dev/handlers/sf_test.sh
+if [ -f "$handlerFile" ]; then
+    $handlerFile $@
+fi
+
 # shellcheck disable=SC2068
 symfony php bin/phpunit $@
